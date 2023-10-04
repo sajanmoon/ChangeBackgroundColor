155fd53e3c6e8aabae84e2ba2c5d1d2bd8708567
sajan moon <sajanmoon5@gmail.com>
2023-10-04 20:14:14 +0530

    changeColors

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,31 @@ import './App.css'
 function App() {
   const [color, setColor] = useState("#fefbf3")
   return (
+    <>
     <div className='bgcolor' style={{ backgroundColor: color }}>
       <div className="container">
         <button onClick={() => setColor("red")}
           className='button red'>RED</button>
+          <button onClick={() => setColor("orange")} className='button orange'>ORANGE</button>
+          <button onClick={() => setColor("yellow")} className='button yellow'>YELLOW</button>
+          <button onClick={() => setColor("green")}
+          className='button green'>GREEN</button>
         <button onClick={() => setColor("blue")}
           className='button blue'>BLUE</button>
-        <button onClick={() => setColor("green")}
-          className='button green'>GREEN</button>
-        <button onClick={() => setColor("yellow")} className='button yellow'>YELLOW</button>
-        <button onClick={() => setColor("pink")} className='button pink'>PINK</button>
-        <button onClick={() => setColor("orange")} className='button orange'>ORANGE</button>
-        <button onClick={() => setColor("purple")} className='button purple'>PURPLE</button>
-        <button onClick={() => setColor("maroon")} className='button maroon'>MAROON</button>
-        <button onClick={() => setColor("black")} className='button black'>BLACK</button>
-
+       
+        
+        <button onClick={() => setColor("indigo")} className='button indigo'>INDIGO</button>
+        <button onClick={() => setColor("violet")} className='button violet'>VIOLET</button>
+      
+        
       </div>
+     
+
     </div>
+     <div className="footer">
+     <p>@sajan_moon</p>
+   </div>
+   </>
   )
 
 }
